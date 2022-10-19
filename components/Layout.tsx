1a1088a76a5d0deb016c2ca886942a4852a7ff87
Fabian Kresler <11368086+fkresler@users.noreply.github.com>
2022-10-19 22:53:20 +0200

    feat: add basic footer markup and styling

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,45 +8,48 @@ type Props = {
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header className={styles.header}>
-      <nav>
-        <div>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </div>
-        <div className={styles.nav_large}>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-          <Link href="/users">
-            <a>Users List</a>
-          </Link>
-          <Link href="/api/users">
-            <a>Users API</a>
-          </Link>
-        </div>
-        <div className={styles.nav_small}>
-          Icon to open a layer
-        </div>
-        <div className={styles.nav_small_popup}>
-          Layered navigation markup
-        </div>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-);
+const Layout = ({ children, title = 'This is the default title' }: Props) => {
+  const currentFullYear = new Date().getFullYear();
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header className={styles.header}>
+        <nav>
+          <div>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </div>
+          <div className={styles.nav_large}>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+            <Link href="/users">
+              <a>Users List</a>
+            </Link>
+            <Link href="/api/users">
+              <a>Users API</a>
+            </Link>
+          </div>
+          <div className={styles.nav_small}>
+            Icon to open a layer
+          </div>
+          <div className={styles.nav_small_popup}>
+            Layered navigation markup
+          </div>
+        </nav>
+      </header>
+      {children}
+      <footer className={styles.footer}>
+        © 2022-{currentFullYear} Fabian Kresler
+      </footer>
+    </div>
+  );
+};
 
 export default Layout;
