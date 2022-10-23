4f38d8f9838f2bd99d4dd6a2d3165489e55580b1
Fabian Kresler <11368086+fkresler@users.noreply.github.com>
2022-10-24 00:33:33 +0200

    feat: add basic contact form styling

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Input, Textarea } from './Input';
+import styles from './ContactForm.module.scss';
 
 const ContactForm = () => {
+  const [name, setName] = React.useState<string>('');
+  const [mail, setMail] = React.useState<string>('');
+  const [message, setMessage] = React.useState<string>('');
+
   return (
-    <form>
-        <label label-for="contact-name">Your name</label>
-        <input id="contact-name" name="contact-name" type="text" />
-        <label label-for="contact-mail">Your mail</label>
-        <input id="contact-mail" name="contact-mail" type="email" />
-        <label label-for="contact-message">Your message</label>
-        <textarea id="contact-message" name="contact-message" />
+    <form className={styles.contactform}>
+      <div className={styles.row}>
+        <Input label="Your name" id="contact-name" type="text" placeholder="Your name" value={name} onChange={(e) => setName(e.target.value)} />
+        <Input label="Your mail" id="contact-mail" type="email" placeholder='Your mail' value={mail} onChange={(e) => setMail(e.target.value)} />
+      </div>
+        <Textarea label="Your message" id="contact-message" placeholder='Your message' value={message} onChange={(e) => setMessage(e.target.value)} />
         <button type="submit">Send</button>
       </form>
   );
---
+++ components/Input.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styles from './Input.module.scss';
+
+type BasicInputProps = {
+  id: string;
+  label: string;
+  value: string;
+  placeholder: string;
+}
+
+type InputProps = BasicInputProps & {
+  type: 'text' | 'email';
+  onChange: (value: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+type TextareaProps = BasicInputProps & {
+  onChange: (value: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+export const Input = ({
+  id, label, value, type, placeholder, onChange
+}: InputProps) => {
+  return (
+    <>
+      <label className={styles.label} label-for={id}>{label}</label>
+      <input className={styles.input} id={id} name={id} type={type} placeholder={placeholder} value={value} onChange={onChange} />
+    </>
+  );
+};
+
+export const Textarea = ({
+  id, label, value, placeholder, onChange
+}: TextareaProps) => {
+  return (
+    <>
+      <label className={styles.label} label-for={id}>{label}</label>
+      <textarea className={styles.input} id={id} name={id} placeholder={placeholder} value={value} onChange={onChange} />
+    </>
+  );
+};
+
+export default Input;
