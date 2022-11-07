4bdbdd2cd2a8489ae7ce691f0670fab8a28ee90d
Fabian Kresler <11368086+fkresler@users.noreply.github.com>
2022-11-07 16:01:49 +0100

    feat: add basic styling variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,34 @@
 import React, { ReactNode } from 'react';
+import Link from 'next/link';
 import '../styles/reset.css';
-import RootLayout from './RootLayout';
+import '../styles/variables.css';
+import styles from './layout.module.scss';
 
 type Props = {
-  children?: ReactNode
-}
+  children?: ReactNode;
+};
 
 const Layout = ({ children }: Props) => {
+  const currentFullYear = new Date().getFullYear();
+
   return (
     <html lang="en">
-      <body>
-        <RootLayout>
-          {children}
-        </RootLayout>
+      <body className={styles.body}>
+        <header className={styles.header}>
+          <nav>
+            <div>
+              <Link href="/">Home</Link>
+            </div>
+            <div className={styles.nav_large}>
+              <Link href="/works">Works</Link>
+              <Link href="/contact">Contact</Link>
+            </div>
+            <div className={styles.nav_small}>Icon to open a layer</div>
+            <div className={styles.nav_small_popup}>Layered navigation markup</div>
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer className={styles.footer}>© 2022-{currentFullYear} Fabian Kresler</footer>
       </body>
     </html>
   );
