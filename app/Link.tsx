b12d4f59a9e9f1e606ea747c533db6fdf471daa5
Fabian Kresler <11368086+fkresler@users.noreply.github.com>
2022-12-02 17:15:06 +0100

    feat: added possibility to provide additional classnames by prop in the Link component

--- app/Link.tsx
+++ app/Link.tsx
@@ -3,8 +3,10 @@ import Link from 'next/link';
 import styles from './Link.module.scss';
 
 const BasicLink = ({ children, ...rest }: ComponentProps<typeof Link>) => {
+  const classNameArray = [styles.link].concat(rest.className ? [rest.className] : []);
+  const classNames = classNameArray.join(' ');
   return (
-    <Link {...rest} className={styles.link}>
+    <Link {...rest} className={classNames}>
       {children}
     </Link>
   );
